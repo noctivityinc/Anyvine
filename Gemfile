a6e75a2788fd68401e4c294a7ae7593e2ae71965
Noctivity Inc <jlippiner@noctivity.com>
2011-07-27 13:45:16 -0400

    initial template creation

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'pg'
+gem 'unicorn'
+gem 'ruby-debug19', :require => 'ruby-debug'
+gem 'nokogiri'
+gem 'haml'
+gem 'compass'
+gem 'nifty-generators'
+gem 'jquery-rails', '>= 1.0.12'
+
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anyvine::Application.config.secret_token = 'c4d03dc1f290a1a267684c63c9face979b10299b414e310b033ad08f3ffdd94ed3a47c44e49baee550972f346b2b3e8006b4d91c3890e4cd49682062a576e2e4'
